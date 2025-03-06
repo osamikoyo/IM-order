120a1b9b2b9e6a637cde1841bdabf8d9e93027f8
osamikoyo <v.pylayev@mail.ru>
2025-03-06 06:37:36 +0500

    add proto

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type Product struct{
 	Name string `bson:"name"`
 	Price uint64 `bson:"price"`
@@ -9,5 +11,6 @@ type Order struct{
 	ID uint64 `bson:"id"`
 	Status string `bson:"status"`
 	Price uint64 `bson:"price"`
+	CreatedAt time.Time `bson:"created_at"`
 	Products []Product `bson:"products"`
 }
