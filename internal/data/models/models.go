ff34885aa6b9eed2d200451cbbeb66510000ca5e
osamikoyo <v.pylayev@mail.ru>
2025-03-06 06:33:54 +0500

    add main models

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,5 +1,13 @@
 package models
 
-type Order struct{
+type Product struct{
+	Name string `bson:"name"`
+	Price uint64 `bson:"price"`
+}
 
+type Order struct{
+	ID uint64 `bson:"id"`
+	Status string `bson:"status"`
+	Price uint64 `bson:"price"`
+	Products []Product `bson:"products"`
 }
