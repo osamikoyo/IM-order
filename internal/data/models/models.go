20c7df95d3090abaa45ff0b76f67f648d34934c6
osamikoyo <v.pylayev@mail.ru>
2025-03-09 11:18:18 +0500

    add helpfuncs

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/osamikoyo/IM-order/pkg/proto/pb"
+)
 
 type Product struct{
 	Name string `bson:"name"`
 	Price uint64 `bson:"price"`
+	ID uint64 `bson:"id"`
 }
 
 type Order struct{
@@ -14,4 +19,27 @@ type Order struct{
 	Price uint64 `bson:"price"`
 	CreatedAt time.Time `bson:"created_at"`
 	Products []Product `bson:"products"`
+}
+
+func ToPbProducts(p []Product) []*pb.Product {
+	var products []*pb.Product
+	for _, v := range p {
+		products = append(products, &pb.Product{
+			Name: v.Name,
+			Price: v.Name,
+			ID: v.ID,
+		})
+	}
+	return products
+}
+
+func ToPb(order *Order) *pb.Order {
+	return &pb.Order{
+		ID: order.ID,
+		UserId: order.UserID,
+		Price: order.Price,
+		Status: order.Status,
+		CreatedAt: order.CreatedAt.GoString(),
+		Prodcuts: ToPbProducts(order.Products),
+	}
 }
